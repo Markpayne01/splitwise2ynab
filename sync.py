c66f0ed7637e692542986046e820bf8d1c975ea0
Mark Payne <markp1989@gmail.com>
2025-02-23 13:53:42 +0000

    pulled the user id from the API instead of hardcoding

--- sync.py
+++ sync.py
@@ -7,7 +7,6 @@ import pytz
 
 # API credentials
 SPLITWISE_API_KEY = os.getenv('SPLITWISE_API_KEY')
-SPLITWISE_USER_ID = int(os.getenv('SPLITWISE_USER_ID'))
 YNAB_ACCESS_TOKEN = os.getenv('YNAB_ACCESS_TOKEN')
 YNAB_BUDGET_ID = os.getenv('YNAB_BUDGET_ID') 
 YNAB_ACCOUNT_ID = os.getenv('YNAB_ACCOUNT_ID') 
@@ -15,11 +14,26 @@ YNAB_ACCOUNT_ID = os.getenv('YNAB_ACCOUNT_ID')
 
 
 # API endpoints
-SPLITWISE_API_URL = 'https://secure.splitwise.com/api/v3.0/get_expenses'
+SPLITWISE_API_URL = 'https://secure.splitwise.com/api/v3.0'
 YNAB_API_URL = f'https://api.youneedabudget.com/v1/budgets/{YNAB_BUDGET_ID}/transactions'
 
 # Timezone settings
 TIMEZONE = pytz.timezone("UTC")  # Set to your timezone if needed
+
+def get_current_splitwise_user_id(): 
+    headers = {
+        'Authorization': f'Bearer {SPLITWISE_API_KEY}',
+        'Content-Type': 'application/json'
+    }  
+
+    response = requests.get(f"{SPLITWISE_API_URL}/get_current_user", headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['user']['id']
+    else:
+        print(f"Failed to fetch user id from Splitwise: {response.status_code}")
+        return []
 
 def get_splitwise_transactions(limit=10, days_ago=2):
     """
@@ -41,7 +55,7 @@ def get_splitwise_transactions(limit=10, days_ago=2):
 
     }
     
-    response = requests.get(SPLITWISE_API_URL, headers=headers, params=params)
+    response = requests.get(f"{SPLITWISE_API_URL}/get_expenses", headers=headers, params=params)
     
     if response.status_code == 200:
         data = response.json()
@@ -50,14 +64,14 @@ def get_splitwise_transactions(limit=10, days_ago=2):
         print(f"Failed to fetch transactions from Splitwise: {response.status_code}")
         return []
 
-def format_for_ynab(transaction):
+def format_for_ynab(transaction,user_id):
     """
     Format Splitwise transaction data for YNAB based on the net_balance for the specific user.
     """
     # Find the user data for the specific USER_ID
     amount = None
     for user in transaction['users']:
-        if user['user']['id'] == SPLITWISE_USER_ID: 
+        if user['user']['id'] == user_id: 
             amount = user['net_balance']
 
     # YNAB expects milliunits, so convert net_balance to milliunits
@@ -96,8 +110,9 @@ def post_transactions_to_ynab(batch):
 
 def import_transactions():
     transactions = get_splitwise_transactions()
+    userid = get_current_splitwise_user_id()
     print(f"Transactions from Splitwise {transactions}")
-    ynab_transactions = [format_for_ynab(transaction) for transaction in transactions]
+    ynab_transactions = [format_for_ynab(transaction,userid) for transaction in transactions]
     print(f"Transactions for ynab {ynab_transactions}")
 
     post_transactions_to_ynab(ynab_transactions)
